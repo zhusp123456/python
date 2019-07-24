023ea5bd8d6a7bfbba82c52ada71db76742f707b
zhusp123456 <1412256442@qq.com>
2019-07-24 22:02:36 +0800

    78953

--- zhusp.py
+++ zhusp.py
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 import time
 def master():
 	print("hello,word!")
 	print(time.time())
     return 0
-=======
-def zhufeixiang():
-	print("zhufeixiang")
-	return 0
 
-
->>>>>>> zhudafei
-
-
+master
 
 
 
