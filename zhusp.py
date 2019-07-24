e04aec67eb5abb6cfa8686c192b1e726507254ab
zhusp123456 <1412256442@qq.com>
2019-07-24 22:32:00 +0800

    9977999777755114444778887777

--- zhusp.py
+++ zhusp.py
@@ -1,22 +1,7 @@
 zhufeixiang
 dengqifang
 duqingsong
-<<<<<<< HEAD
 
-
-=======
-import time
-def master():
-	print("hello,word!")
-	print(time.time())
-    return 0
-
-#邓棋方
-def dengqifang():
-	print("dengqifang")
-	return 0
-	
->>>>>>> dengdafang
 
 
 
